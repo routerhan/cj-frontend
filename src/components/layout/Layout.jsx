08d1f55e9aefd6c9c1661a9c2126fd43f05dcef5
Ching Han Chen <ching.chen@ibm.com>
2025-10-20 16:15:27 +0200

    Revert "chore: Improve the UI"
    This reverts commit 35816c849cd7e9b0a47a0450171c35224aa17d63.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,24 +1,14 @@
-import { useEffect } from 'react'
-import { useFormContext } from '../../context/FormContext.jsx'
 import styles from './Layout.module.css'
 import { Sidebar } from './Sidebar.jsx'
 import { MainContent } from './MainContent.jsx'
 import { MobileStepper } from './MobileStepper.jsx'
 
-export const Layout = ({ children }) => {
-  const { currentStep } = useFormContext()
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }, [currentStep])
-
-  return (
-    <div className={styles.appShell}>
-      <Sidebar />
-      <MainContent>
-        <MobileStepper />
-        <div className={styles.contentArea}>{children}</div>
-      </MainContent>
-    </div>
-  )
-}
+export const Layout = ({ children }) => (
+  <div className={styles.appShell}>
+    <Sidebar />
+    <MainContent>
+      <MobileStepper />
+      <div className={styles.contentArea}>{children}</div>
+    </MainContent>
+  </div>
+)
